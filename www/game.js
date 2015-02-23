e01b6b41eb39ae084a9bb8b62b964de823217aea
Mark Lassoff <mark@learntoprogram.tv>
2015-02-23 16:12:00 -0500

    Welcome Sounds and Level I Screen

--- www/game.js
+++ www/game.js
@@ -29,18 +29,48 @@ function preloadAssets()
         {id: "bt_hole", src:"assets/backgroundTiles/bt_hole.png"},
         {id: "bt_flowerRock", src:"assets/backgroundTiles/bt_flowerRock.png"},
         {id: "bt_rock", src:"assets/backgroundTiles/bt_rock.png"},
-        {id: "bt_flowers", src:"assets/backgroundTiles/bt_flowers.png"}
+        {id: "bt_flowers", src:"assets/backgroundTiles/bt_flowers.png"},
+        {id: "snd_welcome", src:"assets/sounds/welcome.mp3"}
     ]);
 }
 
 function assetsLoaded()
 {
-    alert("assets Loaded");   
+    //Display the Level1 Screen
+    var background = display.queue.getResult("ls_credit");
+    display.stage.addChild(new createjs.Bitmap(background));
+    display.stage.update();
+    
+    //click to start the game
+    display.stage.addEventListener("click", function(event) { loadLevel(1); })
+    
+    //Play welcome music
+    createjs.Sound.play("snd_welcome");
+}
+
+function loadLevel(level)
+{
+    //Stop Sounds
+    createjs.Sound.stop();
+    
+    //Remove Current Click Listener
+    display.stage.removeAllEventListeners();
+    
+    //Display Level Screen
+    display.stage.removeAllChildren();
+    display.stage.update();
+    var level = "ls_level" + level;
+    var level_screen = display.queue.getResult(level);
+    display.stage.addChild(new createjs.Bitmap(level_screen));
+    display.stage.update();
+    
+    
+
 }
 
 function setupCanvas()
 {
     display.stage = new createjs.Stage("myCanvas");
-    display.stage.width = constant.WIDTH;
-    display.stage.height = constant.HEIGHT;
+    display.stage.canvas.width = constant.WIDTH;
+    display.stage.canvas.height = constant.HEIGHT;
 }
